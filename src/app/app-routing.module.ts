d92aec7aa7455a01366919438b8f9f3a0ca82d66
Rojimme <merodriguezji@est.utn.ac.cr>
2023-07-14 23:48:14 -0600

    Laboratorio número 5, sobre el nuevo tema visto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'seguridad', loadChildren: () => import('./pages/seguridad/seguridad.module').then(m => m.SeguridadModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
